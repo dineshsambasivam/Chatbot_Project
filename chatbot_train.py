15ccffe87be1a9fefc3df3af6c560851ff22d222
dinesh s <dineshvlr90@gmail.com>
2018-06-21 12:04:59 +0530

    Update chatbot_train.py

--- chatbot_train.py
+++ chatbot_train.py
@@ -6,8 +6,8 @@ def setup():
     chatbot = ChatBot('Bot',
     storage_adapter='chatterbot.storage.SQLStorageAdapter',
     trainer='chatterbot.trainers.ListTrainer')
-    for file in os.listdir('C:/Users/Yuvashree/Youtube_tutor/Chatbot/data/'):
-        convData = open(r'C:/Users/Yuvashree/Youtube_tutor/Chatbot/data/' + file,encoding='latin-1').readlines()
+    for file in os.listdir('../data/'):
+        convData = open(r'../data/' + file,encoding='latin-1').readlines()
         chatbot.set_trainer(ListTrainer)
         chatbot.train(convData)
         #print("Training completed")
