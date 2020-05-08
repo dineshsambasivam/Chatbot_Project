d8aab70f90be1ea298d7c8a00a86855d6c29611d
dinesh s <dineshvlr90@gmail.com>
2020-05-08 20:08:01 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,5 @@
 from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
+#from chatterbot.trainers import ListTrainer
 
 
 def get_response(usrText):
@@ -7,16 +7,13 @@ def get_response(usrText):
                   storage_adapter='chatterbot.storage.SQLStorageAdapter',
     logic_adapters=[
         {
-            'import_path': 'chatterbot.logic.BestMatch'
-        },
-        {
-            'import_path': 'chatterbot.logic.LowConfidenceAdapter',
-            'threshold': 0.70,
-            'default_response': 'I am sorry, but I do not understand.'
+            "import_path": "chatterbot.logic.BestMatch",
+            "statement_comparison_function": chatterbot.comparisons.levenshtein_distance,
+            "response_selection_method": chatterbot.response_selection.get_first_response
         }
     ],
     trainer='chatterbot.trainers.ListTrainer')
-    bot.set_trainer(ListTrainer)
+    #bot.set_trainer(ListTrainer)
     while True:
         if usrText.strip()!= 'Bye':
             result = bot.get_response(usrText)                        
